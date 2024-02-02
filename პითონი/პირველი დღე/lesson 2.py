42253d04bc3fbc8bb43307d06d8df630f29788b1
unexplainednerd <157606755+unexplainednerd@users.noreply.github.com>
2024-02-02 20:51:46 +0400

    goa homework
    goa homework

---
+++ პითონი/პირველი დღე/lesson 2.py
@@ -0,0 +1,31 @@
+#name = "levani"
+
+
+surname = "begoidze"
+
+#print (name)
+
+name = "im levani begoidze and im"
+age = 11
+height = 134
+
+knows_progrmming = True
+is_ugly = False
+
+
+
+print(name + " " + str(age))
+
+print ("im 135 cm tall")
+
+print ("and i play football")
+
+print ("i want to learn programming")
+
+print ("cuz i like programming")
+
+
+
+
+
+
